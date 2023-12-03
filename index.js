b4269ae5bf4d5b7987d23ba1fe714389fc7265ef
blinkodemia <velah027@gmail.com>
2023-12-03 02:05:09 -0300

    Final touches, along with update

--- index.js
+++ index.js
@@ -2,10 +2,3 @@ function getTime() {
   var dt = new Date();
   document.getElementById("date-time").innerHTML = dt;
 }
-
-function blink() {
-  var x = document.getElementById("blink");
-  setInterval(function () {
-    x.innerHTML = x.innerHTML == "_" ? " " : "_";
-  }, 500);
-}
